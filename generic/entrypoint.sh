7adea8ad3cfd23c1bbfed65314316cab924048c1
Ole Odendahl <oleodendahl@gmail.com>
2019-07-25 10:29:03 +0200

    Fixed entrypoint and Dockerfile for wheel creation.

--- generic/entrypoint.sh
+++ generic/entrypoint.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
-if [ $1 -ne '' ]; then
+if [ $1 != "" ]; then
 
     pip wheel $1
 
-    $created_wheel = find . -name "*.whl" -print0 | xargs -r -0 ls -1 -t | head -1
+    created_wheel=$(find . -name "*.whl" -print0 | xargs -r -0 ls -1 -t | head -1)
+    echo ${created_wheel}
 
     auditwheel repair ${created_wheel}
 else
