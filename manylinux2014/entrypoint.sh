6e1535d7cdfe5cf767f3fd667d8417fd01b4b7b8
Ole Odendahl <odendahl@rzvn.de>
2020-02-15 17:05:39 +0100

    Enhancing entrypoint to accept version as optional parameter

--- manylinux2014/entrypoint.sh
+++ manylinux2014/entrypoint.sh
@@ -2,7 +2,14 @@
 
 if [ $1 != "" ]; then
 
-    pip wheel $1
+    wheel_definition=$1
+
+    if [ $2 != "" ]; then
+        wheel_definition="$1==$2"
+    fi
+
+    echo $wheel_definition
+    pip wheel -v $wheel_definition
 
     created_wheel=$(find . -maxdepth 1 -name "$1*.whl" -print0 | xargs -r -0 ls -1 -t | head -1)
     echo ${created_wheel}
