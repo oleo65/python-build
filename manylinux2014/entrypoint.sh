2ab252b274cdb57d06d903d0afdd85b142fb99f3
Ole Odendahl <oleodendahl@gmail.com>
2020-02-15 17:26:02 +0100

    fixed errors in entrypoint script file. Changed default entrypoint in cffi.

--- manylinux2014/entrypoint.sh
+++ manylinux2014/entrypoint.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-if [ $1 != "" ]; then
+if [ "$1" != "" ]; then
 
     wheel_definition=$1
 
-    if [ $2 != "" ]; then
+    if [ "$2" != "" ]; then
         wheel_definition="$1==$2"
     fi
 
     echo $wheel_definition
-    pip wheel -v $wheel_definition
+    pip wheel $wheel_definition
 
     created_wheel=$(find . -maxdepth 1 -name "$1*.whl" -print0 | xargs -r -0 ls -1 -t | head -1)
     echo ${created_wheel}
