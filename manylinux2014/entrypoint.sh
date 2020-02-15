3f372b44900d8b0c74a880d90a1ac7e0138ea9f1
Ole Odendahl <odendahl@rzvn.de>
2020-02-15 17:45:08 +0100

    Options to entrypoint file to switch to different target platform.

--- manylinux2014/entrypoint.sh
+++ manylinux2014/entrypoint.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
+while getopts ":t:" opt; do
+    case ${opt} in
+        t )
+            auditwheelPlatform="--plat $OPTARG"
+        \? )
+            echo "Invalid option provided."
+        : )
+            echo "No argument provided for $OPTARG"
+            echo "Provide valid auditwheel platform tag."
+            echo "E.g. linux_armv7l or manylinux2014_armv7l" 1>&2
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
 if [ "$1" != "" ]; then
 
     wheel_definition=$1
@@ -14,7 +29,7 @@ if [ "$1" != "" ]; then
     created_wheel=$(find . -maxdepth 1 -name "$1*.whl" -print0 | xargs -r -0 ls -1 -t | head -1)
     echo ${created_wheel}
 
-    auditwheel repair --wheel-dir audited ${created_wheel}
+    auditwheel repair --wheel-dir audited "$auditwheelPlatform" ${created_wheel}
 else
     echo "No package parameter provided."
 fi
