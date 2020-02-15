3438ec7d7a3423433042d8b108b7cea7854306f7
Ole Odendahl <odendahl@rzvn.de>
2020-02-15 16:37:25 +0100

    Modified entrypoint to be more robust with filenames. Changed default output directory of audited wheels.

--- manylinux2014/entrypoint.sh
+++ manylinux2014/entrypoint.sh
@@ -4,10 +4,10 @@ if [ $1 != "" ]; then
 
     pip wheel $1
 
-    created_wheel=$(find . -name "*.whl" -print0 | xargs -r -0 ls -1 -t | head -1)
+    created_wheel=$(find . -maxdepth 1 -name "$1*.whl" -print0 | xargs -r -0 ls -1 -t | head -1)
     echo ${created_wheel}
 
-    auditwheel repair ${created_wheel}
+    auditwheel repair --wheel-dir audited ${created_wheel}
 else
     echo "No package parameter provided."
 fi
