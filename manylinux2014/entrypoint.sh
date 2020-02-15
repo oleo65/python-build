dd4db2de1a00d48a21ee894f4bed24fde25102a8
Ole Odendahl <oleodendahl@gmail.com>
2020-02-15 18:33:34 +0100

    Fixed entrypoint script to deal with no given platform. Adjusted cffi Dockerfile to work with new invocation structure.

--- manylinux2014/entrypoint.sh
+++ manylinux2014/entrypoint.sh
@@ -4,8 +4,11 @@ while getopts ":t:" opt; do
     case ${opt} in
         t )
             auditwheelPlatform="--plat $OPTARG"
+            ;;
         \? )
             echo "Invalid option provided."
+            echo "Usage cmd [-t platform_tag] package_name [version_string]"
+            ;;
         : )
             echo "No argument provided for $OPTARG"
             echo "Provide valid auditwheel platform tag."
@@ -23,13 +26,18 @@ if [ "$1" != "" ]; then
         wheel_definition="$1==$2"
     fi
 
-    echo $wheel_definition
     pip wheel $wheel_definition
 
     created_wheel=$(find . -maxdepth 1 -name "$1*.whl" -print0 | xargs -r -0 ls -1 -t | head -1)
     echo ${created_wheel}
 
-    auditwheel repair --wheel-dir audited "$auditwheelPlatform" ${created_wheel}
+    if [ "$auditwheelPlatform" != "" ]; then
+        auditwheelArguments="$auditwheelPlatform --wheel-dir audited ${created_wheel}"
+    else
+        auditwheelArguments="--wheel-dir audited ${created_wheel}"
+    fi
+echo $auditwheelArguments
+    auditwheel repair $auditwheelArguments
 else
     echo "No package parameter provided."
 fi
