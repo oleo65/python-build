9c7bfdad6dc3d5d846aec1337d094fd62bb1d405
Ole Odendahl <odendahl@rzvn.de>
2020-06-08 10:39:12 +0200

    Slightly improved manylinux image and scripts.

--- manylinux2014/entrypoint.sh
+++ manylinux2014/entrypoint.sh
@@ -28,7 +28,7 @@ if [ "$1" != "" ]; then
 
     pip wheel --no-deps $wheel_definition
 
-    created_wheel=$(find . -maxdepth 1 -name "$1*.whl" -print0 | xargs -r -0 ls -1 -t | head -1)
+    created_wheel=$(find . -maxdepth 1 -iname "$1*.whl" -print0 | xargs -r -0 ls -1 -t | head -1)
     echo ${created_wheel}
 
     if [ "$auditwheelPlatform" != "" ]; then
