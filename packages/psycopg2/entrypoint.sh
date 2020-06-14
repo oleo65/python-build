198681250d11278cee4445518a3200ea65c505ee
Ole Odendahl <odendahl@rzvn.de>
2020-06-14 14:02:17 +0200

    Manually merged changes to psycopg2 pipeline.

--- packages/psycopg2/entrypoint.sh
+++ packages/psycopg2/entrypoint.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 set -e
 
-export PATH=/usr/local/pgsql/bin:$PATH
-
 pip wheel --no-deps --global-option=build_ext --global-option="--static-libpq" psycopg2
 
 auditwheel repair --wheel-dir audited psycopg*.whl
